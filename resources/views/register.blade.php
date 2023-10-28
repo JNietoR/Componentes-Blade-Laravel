dd02697b931a25536b7e0c6f471534d2772b7416
JNietoR <jorge.nieto35@gmail.com>
2023-10-28 11:40:39 +0200

    eliminación vistas predefinidas creación vistas propias

---
+++ resources/views/register.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+    <div class="h-screen flex flex-col justify-center items-center bg-blue-100">
+        <h1 class="mb-6 text-2xl">Crear cuenta</h1>
+        <div class="w-full sm:max-w-md py-4 px-6 bg-white shadow sm:rounded">
+            Contenido
+        </div>
+    </div>
+
+
+</body>
+
+</html>
